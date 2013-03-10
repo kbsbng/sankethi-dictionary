7e8e450a8d66f20d0818d1e23c56fa172f41e12c
Keshavaprasad B S <kbsbng@gmail.com>
2013-03-10 16:52:39 +0530

    Add word page

---
+++ mojits/AddWord/controller.server.js
@@ -0,0 +1,31 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('AddWord', function (Y, NAME) {
+
+    /**
+     * The AddWord module.
+     *
+     * @module AddWord
+     */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function (ac) {
+            ac.assets.addCss('./index.css');
+            ac.done({});
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon', 'AddWordModelFoo']});
---
+++ mojits/AddWordPage/controller.server.js
@@ -0,0 +1,31 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('AddWordPage', function (Y, NAME) {
+
+    /**
+     * The AddWordPage module.
+     *
+     * @module AddWordPage
+     */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function (ac) {
+            ac.assets.addCss('./index.css');
+            ac.composite.done();
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon', 'mojito-composite-addon']});
