b571e494f6038ff656966f8338774c3fb606c158
Keshavaprasad B S <kbsbng@gmail.com>
2013-03-23 22:59:53 +0530

    Jade template working

--- addons/jade.server.js
+++ addons/jade.server.js
@@ -13,25 +13,11 @@ YUI.add('addons-viewengine-jade', function (Y, NAME) {
     JadeAdapter.prototype = {
         render: function (data, mojitType, tmpl, adapter, meta, more) {
             tmpl = tmpl["content-path"];
-//            Y.log("data: " + JSON.stringify(data));
-//            Y.log("mojitType: " + JSON.stringify(mojitType));
-//            Y.log("tmpl: " + JSON.stringify(tmpl));
-//            Y.log("adapter: " + JSON.stringify(adapter));
-//            Y.log("meta: " + JSON.stringify(meta));
             var compiledTmpl = compiledTemplates[tmpl];
             data.meta = meta.view;
             if (compiledTmpl === undefined) {
-//                compiledTemplates[tmpl] = this.compiler(tmpl);
-//                compiledTmpl = compiledTemplates[tmpl];
-//                console.log(compiledTmpl(data));
-//                if (more === true) {
-//                    adapter.flush(compiledTmpl(data), meta);
-//                } else {
-//                    adapter.done(compiledTmpl(data), meta);
-//                }
                 this.compileAsync(tmpl, function (compiledTmpl) {
                     compiledTemplates[tmpl] = compiledTmpl;
-                    console.log(compiledTmpl(data));
                     if (more === true) {
                         adapter.flush(compiledTmpl(data), meta);
                     } else {
@@ -40,19 +26,11 @@ YUI.add('addons-viewengine-jade', function (Y, NAME) {
                 });
                 return;
             }
-            console.log(compiledTmpl(data));
             if (more === true) {
-                Y.log("Flushing");
                 adapter.flush(compiledTmpl(data), meta);
             } else {
-                Y.log("Jade templ done");
                 adapter.done(compiledTmpl(data), meta);
             }
-        },
-        compiler: function (tmpl) {
-            Y.log("jade compiler called", "debug", NAME);
-            return jade.compile(fs.readFileSync(tmpl, 'utf8'), {filename: tmpl});
-            //return fs.readFileSync(tmpl, 'utf8');
         },
         compileAsync: function (tmpl, cb) {
             Y.log("Asynchrounous compile for jade tmpl called: " + JSON.stringify(tmpl), "debug", NAME);
@@ -60,7 +38,6 @@ YUI.add('addons-viewengine-jade', function (Y, NAME) {
                 if (err) {
                     throw err;
                 }
-                console.log(data);
                 cb(jade.compile(data, {filename: tmpl}));
             });
         }
