a63525a5371361ff6faf04a1eb22027e826b8e65
Keshavaprasad B S <kbsbng@gmail.com>
2020-11-11 16:00:36 +0530

    update mongo db

--- autoload/mongo.server.js
+++ autoload/mongo.server.js
@@ -2,7 +2,7 @@ YUI.add('sankethi-dictionary-mongo', function (Y) {
     "use strict";
     var mongo, db, dictionary, users;
     mongo = require('mongoskin');
-    db = mongo.db(process.env.MONGOHQ_URL || "mongodb://sanketidictionary:sanketidictionaryreadonly@ds231739.mlab.com:31739/heroku_6rcwvxcv", {
+    db = mongo.db(process.env.MONGOHQ_URL || "mongodb://sanketidictionary:sanketidictionaryreadonly@msscombocluster-shard-00-00.atj3m.mongodb.net:27017,msscombocluster-shard-00-01.atj3m.mongodb.net:27017,msscombocluster-shard-00-02.atj3m.mongodb.net:27017/heroku_6rcwvxcv?ssl=true&replicaSet=atlas-3tzz6w-shard-0&authSource=admin&retryWrites=true&w=majority", {
         safe: true
     });
     dictionary = db.collection("dictionary");
